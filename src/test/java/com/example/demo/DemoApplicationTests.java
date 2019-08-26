997ccdf8d6d8c04604f2a1350166ebc03aa28b84
rockychen <rockychen1221@163.com>
2019-08-26 19:03:36 +0800

    remove Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.model.FactWeather;
 import com.example.demo.service.FactWeatherService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,33 +13,4 @@ public class DemoApplicationTests {
 	@Autowired
 	private FactWeatherService factWeatherService;
 
-	@Test
-	public void contextLoads() {
-		System.out.println(factWeatherService.count());
-	}
-
-	@Test
-	public void testInsert() {
-
-	}
-
-	@Test
-	public void testGetAll() {
-		Iterable<FactWeather> iterable = factWeatherService.getAll();
-		iterable.forEach(e->System.out.println(e.toString()));
-	}
-
-	@Test
-	public void testGetByName() {
-		List<FactWeather> list = factWeatherService.getByArea("北京");
-		System.out.println(list);
-	}
-
-	@Test
-	public void testPage() {
-		Page<FactWeather> page = factWeatherService.pageQuery(0, 10, "北京");
-		System.out.println(page.getTotalPages());
-		System.out.println(page.getNumber());
-		System.out.println(page.getContent());
-	}
 }
